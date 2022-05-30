9b9499b369d9ec1e6dc5df88353377c986204c28
Hacca13 <V.Noviello3@gmail.com>
2022-05-30 15:51:41 +0200

    Implements Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express"
 import "express-async-errors"
 import swaggerUi from "swagger-ui-express"
-//import { swaggerDocument } from "./swagger"
+import { swaggerDocument } from "./swagger"
 
 import { userRoutes } from "./routes"
 import { errorMessage } from "./middleware"
@@ -9,7 +9,7 @@ import { errorMessage } from "./middleware"
 export const setupServer = (): Express => {
   const app: Express = express()
 
-  //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use(express.json())
   app.use("/", userRoutes)
